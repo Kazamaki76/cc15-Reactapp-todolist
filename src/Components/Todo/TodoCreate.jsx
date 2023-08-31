8740f47c1e9f5baab79ff8cd1c0a2a112d00a117
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-08-31 17:09:00 +0700

    Update data

--- src/Components/Todo/TodoCreate.jsx
+++ src/Components/Todo/TodoCreate.jsx
@@ -1,16 +1,86 @@
 import styles from './TodoCreate.module.scss';
+import TodoForm from "./TodoForm" ;
 import { FaPlus } from 'react-icons/fa';
 import { HiPlus } from 'react-icons/hi';
+import { useState } from 'react';
 
-function TodoCreate() {
+
+/*
+
+
+CC1 Condition rendering 
+-default : show button & Text
+-active : show TodoForm 
+
+_Concept : true ? <AddTask> : <AddForm>
+*/
+
+/*
+CC2 Event Handling
+-เอา Fn ไปผูกกับ UI เพื่อให้ user เรียกใช้ FN เอง 
+-onClick : ต้องคลิ๊กก่อน, FN ทำงาน 
+  -User คลิ๊ก
+  -Browser จะเป็นคนเรียกใช้งาน FN โดยส่ง parameter มา 1 ตัว
+  Browser : handleClick(eventObject)
+*/
+
+/*
+CC3- JS value ไม่สามารถทำให้  React rerender ได้ 
+ต้องใช้ state
+โดยการ import useState จาก react libary
+*/
+
+
+
+
+
+/*
+CC4 Array Destuctureing 
+function myUseState(){
+  return[5,9]
+}
+*/
+
+/*
+CC5 React state  ( 1 ใน fn ของกลุ่ม React Hook)
+const [state, setState ] = useState(initialState:any)
+//elemtn 1 : current state 
+//element 2 :  FN สำหรับsetState
+// เมื่ิอ state เปลี่ยน function component จะ rerender 
+*/
+
+// 1 FN component  ทำหน้าที่ render ในreact
+function TodoCreate() {   
+  // hook FN
+  const [isOpenForm,setIsOpenForm] = useState(false);
+  console.log(isOpenForm);
+
+  let active = true;
+
+  // 2 JS FN (Logic)
+  const handleClick = function (event) { 
+    console.log(event) ;
+    setIsOpenForm(!isOpenForm)
+  };
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+        <TodoForm 
+        textSubmit="Add Task"
+        setIsOpenForm={setIsOpenForm}
+        />
+      ) : (
+        <div className={styles.todo__create} onClick={handleClick}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
+    </>
   );
 }
+
+
 
 export default TodoCreate;
--- src/Components/Todo/TodoLists.jsx
+++ src/Components/Todo/TodoLists.jsx
@@ -1,27 +1,53 @@
-import styles from './TodoLists.module.scss';
+import { useState } from 'react';
 import { FaTrashAlt, FaPen } from 'react-icons/fa';
 import { HiOutlineCheck } from 'react-icons/hi';
 
+
+import TodoItem from "./TodoItem"
+import TodoForm from './TodoForm';
+import styles from './TodoLists.module.scss';
+
+
+/*
+
+schema
+
+todoObject : {id:number, task: string, status:boolean, due_data:string}
+data = Array<{id:number, task:string, status: boolean , due_date:string }>
+
+dataRender = Array[]<TOdoItem task=... done=... />
+*/
+
+
+const data =[ 
+  { "id": 1, "task": "Suspendisse potenti.", "status": false, "due_date": "2023-04-26" },
+  {
+      "id": 2,
+      "task": "In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+      "status": false,
+      "due_date": "2023-05-08"
+  },
+  {
+      "id": 3,
+      "task": "Aenean fermentum. Donec ut mauris eget massa tempor convallis.",
+      "status": false,
+      "due_date": "2023-04-30"
+  },
+]
+
+
 function TodoLists() {
-  return (
+  const dataRender = data.map((todoObj)=><TodoItem task={todoObj.task} done={todoObj.status} date={todoObj.due_date}/>);
+  const [isOpenForm, setIsOpenForm] = useState(false);
+
+  const handleClick = function () {
+  };
+
+  return ( 
     <ul className={styles.todo__lists}>
-      <li className={styles.todo}>
-        <div className={styles.todo__checkbox}>
-          <HiOutlineCheck className={styles.todo__checkbox__icon} />
-        </div>
-        <p className={styles.todo__task}>todo-item 1 </p>
-        <span className={styles.todo__date}>30 Aug</span>
-        <div className={styles.todo__action}>
-          <span>
-            <FaPen className={styles.todo__edit} />
-          </span>
-          <span>
-            <FaTrashAlt className={styles.todo__delete} />
-          </span>
-        </div>
-      </li>
+     {dataRender}
     </ul>
-  );
+  ) 
 }
 
 export default TodoLists;
